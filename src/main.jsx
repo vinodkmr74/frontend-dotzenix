9eeb7609cb6b6618eaa4345dc5f5f0391d598955
Vinod Kumar <vinod stm74@gmail.com>
2026-04-09 21:13:18 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 
 import "./index.css"
 import "../src/css/style.css"
-import Router from "./router"
+import Router from "./routers"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router />
